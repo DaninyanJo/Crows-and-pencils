b9c261f1d3ff51f7112fc2db5ef4978ece89705e
Daniel <55189660+DaninyanJo@users.noreply.github.com>
2020-09-07 00:15:45 +0300

    reborn

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-let vorona = ['01', '21','31','41','51','61','71','81','91','101']
-let voroni = ['02','03','04',22,23,24,32,33,34,42,43,44,52,53,54,62,63,64,72,73,74,82,83,84,92,93,94]
-let voron  = ['00','05','06','07','08','09',10,11,12,13,14,15,16,17,18,19,20,25,26,27,28,29,30,35,36,37,38,39,40,45,46,47,48,49,50,55,56,57,58,59,60,65,66,67,68,69,70,75,76,77,78,79,80,85,86,87,88,89,90,95,96,97,98,99]
-function crowsAndPencils(n) {
-  n += "";
-  if (n < 10 && n > -10) {
-    let newNum = '0' + Math.abs(n)
-    if (vorona.find((el) => el == newNum)) {
-      return n + " ворона ";
-    } else if (voroni.find((el) => el == newNum)) {
-      return n + " вороны";
-    } else if (voron.find((el) => el == newNum)) {
-      return n + " ворон";
-    }
-  } else {
-    let arr = n.split("");
-    let newNum = arr[arr.length - 2] + arr[arr.length - 1];
-    if (vorona.find((el) => el == newNum)) {
-      return n + " ворона";
-    } else if (voroni.find((el) => el == newNum)) {
-      return n + " вороны";
-    } else if (voron.find((el) => el == newNum)) {
-      return n + " ворон";
-    }
+
+let arr1 = [10 ,11 , 12 , 13 , 14]
+let arr2 = [5 , 6 , 7 , 8 , 9]
+let arr3 = [2 , 3 , 4]
+function getEnding(num) {
+  if ( arr1.find(i => i == num % 100)) {
+    return num + " ворон";
+  } else if (arr2.find(i => i == num % 10)) {
+    return num + " ворон";
+  } else if (arr3.find(i => i == num % 10)) {
+    return num + " вороны";
+  } else if (num % 10 == 1) {
+    return num + " ворона";
+  } else if (num % 10 == 0) {
+    return num + " ворон";
   }
 }
-console.log(crowsAndPencils(0));
-console.log(crowsAndPencils(1));
-console.log(crowsAndPencils(2));
-console.log(crowsAndPencils(5));
-console.log(crowsAndPencils(101));
-console.log(crowsAndPencils(-1));
-console.log(crowsAndPencils(-5));
-console.log(crowsAndPencils(-1862589));
-console.log(crowsAndPencils(-232140000));
-console.log(crowsAndPencils(-542142101));
+console.log(getEnding(0))
